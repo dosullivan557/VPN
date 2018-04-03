a1b40b9f4b1469a3b39fd18e4d4daf691fc56e00
Dan <dosullivan557@gmail.com>
2018-04-03 19:40:48 +0100

    Added file

---
+++ VPN/MainView.swift
@@ -0,0 +1,13 @@
+//
+//  MainView.swift
+//  VPN
+//
+//  Created by Danny on 03/04/2018.
+//  Copyright © 2018 Danny. All rights reserved.
+//
+
+import Cocoa
+
+class MainView: UIViewController {
+
+}
