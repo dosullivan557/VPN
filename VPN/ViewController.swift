0b933833c1129929d78b93589efec7a5203a2833
Dan <dosullivan557@gmail.com>
2018-04-03 18:43:36 +0100

    Initial Commit

---
+++ VPN/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  VPN
+//
+//  Created by Danny on 03/04/2018.
+//  Copyright © 2018 Danny. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
