34072d1a0b0913fa172cff82155def4d7cfa00af
Dan <dosullivan557@gmail.com>
2018-04-03 19:36:18 +0100

    Added button changes

--- VPN/ViewController.swift
+++ VPN/ViewController.swift
@@ -7,21 +7,56 @@
 //
 
 import Cocoa
+import NetworkExtension
+
 
 class ViewController: NSViewController {
+    var vpnRunning = false
+    var manager = NEVPNManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    func startVPN() {
 
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
+        vpnRunning.negate()
+        button.title = "Stop VPN"
+        do {
+            try manager.connection.startVPNTunnel()
+        }
+        catch {
+            print("error")
+        }
+//        manager.loadFromPreferences { (Error) in
+//            print(Error)
+//        }
+    }
+    
+    func stopVPN() {
+        print("Stopping")
+        vpnRunning.negate()
+        button.title = "Start VPN"
+    }
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        if vpnRunning {
+            stopVPN()
+        }
+        else {
+            startVPN()
         }
     }
-
-
+    @IBOutlet weak var button: NSButton!
+    
+    override var representedObject: Any? {
+        didSet {
+            // Update the view, if already loaded.
+        }
+    }
+    
+    
 }
 
