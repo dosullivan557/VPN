7f427e3f322007fded6b4136189e73d254aa4060
Dan <dosullivan557@gmail.com>
2018-04-03 19:36:06 +0100

    Initial Commit

---
+++ Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  Extensions.swift
+//  VPN
+//
+//  Created by Danny on 03/04/2018.
+//  Copyright © 2018 Danny. All rights reserved.
+//
+
+import Foundation
+
+extension Bool {
+    mutating func negate() {
+        self = !self
+    }
+}
